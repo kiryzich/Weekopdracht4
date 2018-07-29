86e0d331b5ed68dcb9a0ddafe620cedd385fae0c
Arne van Buel <arnevanbuel@hotmail.nl>
2018-07-29 12:31:48 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+package Qien.Weekopdracht4;
+
+class Klantreis{
+
+}
+class Fase1{ //orientatie
+    // methodes: chat/onderzoek/praten/reviews/zelftest // schrijven
+
+}
+class Fase2{ //beslis
+    // methodes:
+
+}
+class Fase3{ //wacht
+
+}
+class Fase4{ //start
+
+}
+class Fase5{ //behandeling
+
+}
+class Fase6{ //genezen
+
+}
+
+class Klant{
+    int leeftijd;
+    String geslacht;
+    Klant nieuw (Klant leeftijd, Klant geslacht){
+//        geslacht = inputA;
+//        leeftijd = inputB;
+
+        return new Klant();
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
+class Overslaan{
+    void slaFaseOver(){
+        System.out.println("U heeft nog niet alle opdrachten afgerond, wilt u toch doorgaan?");
+
+    }
+}
