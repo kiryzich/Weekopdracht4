067d898a0769d604ef7f81232131be410648d2a2
Arne van Buel <arnevanbuel@hotmail.nl>
2018-07-29 13:29:53 +0200

    Update 2
    Class KLant aangevukld en tot nu toe werkend

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 package Qien.Weekopdracht4;
 
+import java.util.Scanner;
+
 class Klantreis{
 
 }
 class Fase1{ //orientatie
-    // methodes: chat/onderzoek/praten/reviews/zelftest // schrijven
-
+    // methodes: chat/onderzoek/praten/reviews/zelftest // schrijven test
+    int chatten(){ // void naar int!
+        int x = 0;
+        return x;
+    }
 }
 class Fase2{ //beslis
     // methodes:
@@ -25,13 +30,33 @@ class Fase6{ //genezen
 }
 
 class Klant{
-    int leeftijd;
-    String geslacht;
-    Klant nieuw (Klant leeftijd, Klant geslacht){
-//        geslacht = inputA;
-//        leeftijd = inputB;
+    int[][] arr = new int[5][6];
+    /*  gevulde array geeft:
+        Fase 1  [chat, onderzoek, praten, reviews, zelftest]
+        Fase 2  []
+        Fase 3  []
+        Fase 4  []
+        Fase 5  []
+        Fase 6  []
+     */
 
+    int leeftijd = 0;
+    String geslacht = "Man";
+    static Klant nieuw (int klantID){
         return new Klant();
+    }
+
+    int leeftijdBepalen(int leeftijd){
+        Scanner inputB = new Scanner(System.in);
+        int i = inputB.nextInt();
+        leeftijd = i;
+        return this.leeftijd;
+    }
+    String geslachtBepalen(String s){
+        Scanner inputA = new Scanner(System.in);
+        String i = inputA.nextLine();
+        geslacht = i;
+        return (this.geslacht);
     }
 
 }
@@ -39,12 +64,19 @@ class Klant{
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // vullen
+        Scanner inputA = new Scanner(System.in);
+        String u = "Man";
+        int z = 0;
+        ++z;
+        Klant x = Klant.nieuw(z);
+        x.leeftijdBepalen(z);
+        x.geslachtBepalen(u);
     }
 }
 
 class Overslaan{
-    void slaFaseOver(){
+    void overrideSlaFaseOver(){
         System.out.println("U heeft nog niet alle opdrachten afgerond, wilt u toch doorgaan?");
 
     }
