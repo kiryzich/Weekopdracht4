42e8b70f09490216b790b8fadbd017c634ab7c7d
Arne van Buel <arnevanbuel@hotmail.nl>
2018-07-29 13:53:12 +0200

    Update 4
    Even mn laptop laten afkoelen

--- Main.java
+++ Main.java
@@ -41,22 +41,40 @@ class Klant{
      */
 
     int leeftijd = 0;
-    String geslacht = "Man";
+    String geslacht = "onbekend";
+    String naam = "onbekend";
     static Klant nieuw (int klantID){
         return new Klant();
     }
 
     int leeftijdBepalen(int leeftijd){
+//        System.out.println("De leeftijd is " +leeftijd); //Was een check
+        System.out.println("Om u het beste van dienst te kunnen zijn stellen wij even wat korte vragen.");
+        System.out.println("Wat is uw leeftijd?");
         Scanner inputB = new Scanner(System.in);
         int i = inputB.nextInt();
-        leeftijd = i;
+        this.leeftijd = i;
         return this.leeftijd;
     }
     String geslachtBepalen(String s){
+//        System.out.println("Het geslacht is " +geslacht); //Was een check
+        System.out.println("Wat is uw geslacht?");
         Scanner inputA = new Scanner(System.in);
         String i = inputA.nextLine();
-        geslacht = i;
+        this.geslacht = i;
         return (this.geslacht);
+    }
+
+    String naamBepalen(String s){
+//        System.out.println("Het geslacht is " +geslacht); //Was een check
+        System.out.println("Wat is uw naam?");
+        Scanner inputA = new Scanner(System.in);
+        String i = inputA.nextLine();
+        this.naam = i;
+        return (this.naam);
+    }
+    void voorstellen(){
+        System.out.println(" Hallo " + naam + ",\n u bent " +leeftijd+ " jaar oud, \n en u bent een " +geslacht +"."); // CHECK!
     }
 
 }
@@ -66,12 +84,14 @@ public class Main {
     public static void main(String[] args) {
         // vullen
         Scanner inputA = new Scanner(System.in);
-        String u = "Man";
-        int z = 0;
+        String u = " - ";
+        int z = -1;
         ++z;
         Klant x = Klant.nieuw(z);
         x.leeftijdBepalen(z);
         x.geslachtBepalen(u);
+        x.naamBepalen(u);
+        x.voorstellen();
     }
 }
 
